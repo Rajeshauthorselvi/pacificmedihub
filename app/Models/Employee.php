8686a3d6b66df566b475686460085a2e896ea5a3
rvnaveen <rnaveen@authorselvi.com>
2021-01-19 11:58:53 +0530

    Employee Module Edit from completed

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    public function department()
+    {
+    	return $this->belongsTo('App\Models\Department','emp_department');
+    }
+
+    public function city()
+    {
+    	return $this->belongsTo('App\Models\City','emp_city');
+    }
 }
