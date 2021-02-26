756b4833da4beb4be5422d30b166ae485b436c98
rvnaveen <rnaveen@authorselvi.com>
2021-02-26 19:04:50 +0530

    RFQ view design issues fixed

--- app/Http/Controllers/front/HomePageController.php
+++ app/Http/Controllers/front/HomePageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\front;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Categories;
+use App\Models\Product;
 
 class HomePageController extends Controller
 {
@@ -12,6 +13,8 @@ class HomePageController extends Controller
     {
     	$data = array();
     	$data['categories'] = Categories::where('published',1)->where('is_deleted',0)->get();
+    	$data['products'] = Product::where('published',1)->where('show_home',1)->where('is_deleted',0)->whereMonth('created_at',date('m'))->limit(10)->get();
+    	//dd($data);
     	return view('front.home.home',$data);
     }
 }
