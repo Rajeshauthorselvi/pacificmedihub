1f2c11e4fddd5b72431c332e8ed7f7b19967fb3d
rvnaveen <rnaveen@authorselvi.com>
2021-04-13 17:28:06 +0530

    bug fix

--- app/Http/Controllers/front/HomePageController.php
+++ app/Http/Controllers/front/HomePageController.php
@@ -78,12 +78,12 @@ class HomePageController extends Controller
     public function search(Request $request)
     {
     	if($request->catgory_id==0){
-        	$get_product = Product::where('name','like','%'.$request->search_text.'%')->where('published',1)->where('show_home',1)->where('is_deleted',0)->limit(10)->get();
+        	$get_product = Product::where('name','like','%'.$request->search_text.'%')->where('published',1)->->where('is_deleted',0)->limit(10)->get();
     	}else{
         	$get_product = DB::table('categories as c')->leftJoin('products as p','c.id','p.category_id')
-        				 ->where('c.id',$request->catgory_id)->orWhere('c.parent_category_id',$request->catgory_id)
-        				 ->where('p.name','like','%'.$request->search_text.'%')->where('p.published',1)
-        				 ->where('p.show_home',1)->where('p.is_deleted',0)->limit(10)->get();
+        				->where('c.id',$request->catgory_id)->orWhere('c.parent_category_id',$request->catgory_id)
+        				->where('p.name','like','%'.$request->search_text.'%')->where('p.published',1)
+        				->where('p.is_deleted',0)->limit(10)->get();
         }
         $output=array();
         if(count($get_product)!=0){
