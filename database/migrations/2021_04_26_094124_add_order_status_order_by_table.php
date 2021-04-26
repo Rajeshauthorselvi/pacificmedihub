051ab70be88b49b7ae98c7c0315938c793ab1fdf
Dhinesh <dhinesh@authorselvi.com>
2021-04-26 15:32:52 +0530

    Add order status

--- database/migrations/2021_04_26_094124_add_order_status_order_by_table.php
+++ database/migrations/2021_04_26_094124_add_order_status_order_by_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use App\Models\OrderStatus;
 class AddOrderStatusOrderByTable extends Migration
 {
     /**
@@ -16,6 +16,11 @@ class AddOrderStatusOrderByTable extends Migration
         Schema::table('order_status', function (Blueprint $table) {
            $table->integer('order_by')->after('color_codes');
         });
+        OrderStatus::where('id',18)->update(['order_by'=>2]);
+        OrderStatus::where('id',19)->update(['order_by'=>1]);
+        OrderStatus::where('id',20)->update(['order_by'=>3]);
+        OrderStatus::where('id',21)->update(['order_by'=>4]);
+
     }
 
     /**
