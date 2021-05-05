57a20f97d7d6fc9fa0c36aebf39f8d95e9967972
Dhinesh <dhinesh@authorselvi.com>
2021-05-05 16:26:28 +0530

    Change modal size in stock list

--- resources/views/admin/stock/stock_list/index.blade.php
+++ resources/views/admin/stock/stock_list/index.blade.php
@@ -68,7 +68,7 @@
   </div>
   {{-- Quantity List --}}
     <div class="modal fade" id="show_products" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-dialog modal-xl" role="document">
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="exampleModalLabel">Quantity List</h5>
